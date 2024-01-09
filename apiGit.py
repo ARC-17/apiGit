22992ceee19c26256bb03f992f129185e2fbd766
arc-17 <ti.rarcanjo@gmail.com>
2024-01-09 16:16:47 -0300

    Added more fields about the user

--- apiGit.py
+++ apiGit.py
@@ -10,10 +10,14 @@ response = requests.get(url)
 data = response.json()
 
 if response.status_code == 200:
-    print(f'\nFull name: {data["name"]}')
+    print(f'\nName: {data["name"]}')
     print(f'Bio: {data["bio"]}')
+    print(f'id: {data["id"]}')
     print(f'Location: {data["location"]}')
+    print(f'avatar_url: {data["avatar_url"]}')
     print(f'Followers: {data["followers"]}')
     print(f'Following: {data["following"]}')
+    print(f'email: {data["email"]}')
+    print(f'location: {data["location"]}')
 else:
     print('Could not find user!')
